f5e6d9456b7c915fce4c270e7c7b8ae0d65e7cdd
kapitanzdun <99537596+kapitanzdun@users.noreply.github.com>
2022-03-18 19:56:47 +0300

    вторые шаги
    дорожечка

---
+++ obstacle course/Assets/move_player.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class move_player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hello worlds");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
