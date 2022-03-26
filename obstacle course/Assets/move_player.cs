9b5cc70d439cbc0f3a2555771803dd5fa0443990
kapitanzdun <DANIILZHD.07@MAIL.RU>
2022-03-26 15:38:04 +0300

    5-ые шаги
    всё

--- obstacle course/Assets/move_player.cs
+++ obstacle course/Assets/move_player.cs
@@ -4,12 +4,28 @@ public class move_player : MonoBehaviour
 {
     public Rigidbody Rb;
 
-    public float forwardForse = 5f ; 
+    public float forwardForse = 2000f ;
+    public float sidewaysForse = 500f;
 
     // Update is called once per frame
     void FixedUpdate ()
     {
         Rb.AddForce(forwardForse , 0, 0 * Time.deltaTime);
+
+        if(Input.GetKey("d"))
+        {
+            Rb.AddForce(0,0,-sidewaysForse* Time.deltaTime);
+        }
+
+        if(Input.GetKey("a"))
+        {
+            Rb.AddForce(0,0,sidewaysForse* Time.deltaTime);
+        }
+
+        if(Input.GetKey("w"))
+        {
+            Rb.AddForce(0,100+sidewaysForse* Time.deltaTime,0);
+        }
     }
 }
 
---
+++ obstacle course/Assets/игрокCollision.cs
@@ -0,0 +1,10 @@
+
+using UnityEngine;
+
+public class игрокCollision : MonoBehaviour
+{
+   void OnCollisionEnter ()
+   {
+       Debug.Log("Oh I bumped the owner ^^ .");
+   }
+}
