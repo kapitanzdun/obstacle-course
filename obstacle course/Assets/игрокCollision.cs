28c3930835cee1356e80d7760c1e9231da563ac1
kapitanzdun <99537596+kapitanzdun@users.noreply.github.com>
2022-04-02 18:05:59 +0300

    полу8-ые шаги
    остановка на восьмом EO

--- obstacle course/Assets/игрокCollision.cs
+++ obstacle course/Assets/игрокCollision.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 public class игрокCollision : MonoBehaviour
 
  {
-   public move_player movement ; 
-
+   public move_player movement; 
+   public GameManager GameManager;
    void OnCollisionEnter (Collision collisionInfo )
    {
        if (collisionInfo.collider.tag == "ф")
